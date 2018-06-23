f5f7162ca8c79cdeca27c04b12554ca16485123b
lndillon <lnoeldillon@gmail.com>
2018-06-23 12:33:08 -0400

    starting routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
---
+++ routes/api/index.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const giftRoutes = require("./gifts");
+const requestRoutes = require("./requests");
+const responseRoutes = require("./responses");
+
+// Gift routes
+router.use("/gifts", giftRoutes);
+
+// Response routes
+router.use("/requests", requestRoutes);
+
+
+// Gift routes
+router.use("/responses", responseRoutes);
+
+
+module.exports = router;
