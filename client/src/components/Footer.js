0f75c1beba920882fb48661cbbb057fb90a596de
lndillon <lnoeldillon@gmail.com>
2018-06-27 22:22:50 -0400

    components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="text-center">
+      <span className="text-muted"><a href="https://github.com/TeamGyfty/Gyfty">GYFTY Repo</a></span>
+    </div>
+  </footer>
+);
+
+export default Footer;
