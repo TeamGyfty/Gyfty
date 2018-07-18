3eb6ae0c6a2ffb8867ae98d5efeaaa966b156bb3
lndillon <lnoeldillon@gmail.com>
2018-07-17 20:14:25 -0400

    test

--- models/index.js
+++
@@ -1,8 +0,0 @@
-//exporting an object containing all of our models
-
-module.exports = {
-    Gift: require("./request"),
-    requiredGiftdata: require("./requiredGift"),
-    User: require("./user"),
-    giftSuggestion: require("./response")
-};
